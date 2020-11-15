2bca1552eb5035706c2036822672f5e4e3900cc5
iaslyk <73584380+iaslyk@users.noreply.github.com>
2020-11-15 13:29:04 +0100

    Update towerofhanoi.py

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -1,22 +1,22 @@
 import time
 
-number = int(input('Enter number of disks: '))
-start = 'A'
-via = 'B'
-finish = 'C'
+number = int(input('Enter number of disks: ')) # number of disks in tower
+start = 'A' # start location
+via = 'B' # helper
+finish = 'C' # end location
 
-def move(f,t):
-	print('Move disk for {} to {}!'.format(f,t))
-	time.sleep(0.3)
+def move(f, t):
+	print('Move disk for {} to {}!'.format(f,t)) # prints how we need to move disks to solve tower
+	time.sleep(0.3) #
 
 
-
+# recursion where n is number of disks in tower, f is starting location, f is helper location, and t is end location
 def hanoi(n, f, v, t):
 	if n == 0:
 		pass
 	else:		
 		hanoi(n-1, f, t, v)
-		move(f,t)
-		hanoi(n-1,v, f, t)
+		move(f, t)
+		hanoi(n-1, v, f, t) 
 
 hanoi(number, start, via, finish)
